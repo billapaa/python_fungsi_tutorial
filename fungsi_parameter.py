b980461fec651824eb3cdfea94920e7a51041466
nabilla <nblputriarisanti2@gmail.com>
2022-05-27 15:44:39 +0700

    parameter

---
+++ fungsi_parameter.py
@@ -0,0 +1,12 @@
+def selamat_datang(nama):
+
+    print('halo', nama , 'apa kabar?')
+
+selamat_datang ('budi')
+
+def greetings(nama, waktu):
+    print('helo', nama , 'selamat', waktu)
+
+greetings('andi', 'pagi')
+greetings('rudi', 'siang')
+greetings('wahyu', 'malam')
